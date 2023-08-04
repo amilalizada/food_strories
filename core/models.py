ee665ef1ce2c72cc0332e214e8564888d9ba9cd5
amilalizada <elizade_amil@inbox.ru>
2023-08-04 22:08:34 +0400

    model test

--- core/models.py
+++ core/models.py
@@ -22,3 +22,6 @@ class Contact(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+    def __repr__(self) -> str:
+        return self.name
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
-
+from .models import Contact
 # Create your tests here.
+
+class TestContactModel(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.cls_atomics = cls._enter_atomics()
+        cls.data = {
+            "name": "Amil",
+            "email": "amil@gmail.com",
+            "subject": "Subject",
+            "message": "Sayt ishlemir!"
+        }
+
+        Contact.objects.create(**cls.data)
+
+    def test_creation(self):
+        contact_obj = Contact.objects.first()
+
+        self.assertEqual(self.data["name"], contact_obj.name)
+
+    def test_str_method(self):
+        contact_obj = Contact.objects.first()
+        self.assertEqual(str(contact_obj), self.data["name"])
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        Contact.objects.first().delete()
+        del cls.data
+
+
